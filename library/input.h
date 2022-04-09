25e9ddc76d129e1c8318c98abbfe5618b57946d8
Muhammad Irfan Rafi Aqdillah <101859452+2100018058@users.noreply.github.com>
2022-04-09 09:12:49 +0700

    Add files via upload

---
+++ library/input.h
@@ -0,0 +1,41 @@
+using namespace std;
+
+class Input{
+  public:
+    void cetak()
+	  {
+      cout<<"Sebuah layanan cloud menyediakan hosting dengan beberapa menu sebagai berikut:"<<endl;
+      cout<<"====================================" <<endl;
+      cout<<"= 1) Shared Hosting     : 300 ribu =" <<endl;
+      cout<<"= 2) VPS Server         : 930 ribu =" <<endl;
+      cout<<"= 3) Cloud Server       : 5 juta   =" <<endl;
+      cout<<"= 4) Domain Name        : 280 ribu =" <<endl;
+      cout<<"====================================" <<endl;
+      cout<< "Pesan Shared Hosting	: "	;cin >> beli1;
+      cout<< "Pesan VPS Server		: "		;cin >> beli2;
+      cout<< "Pesan Cloud Server		: "	;cin >> beli3;
+      cout<< "Pesan Domain Name		: "		;cin >> beli4;
+      cout<<"====================================" <<endl;
+      
+      cout<< "\nApakah Anda Pembeli Dari Indonesia?"<<endl;
+      cout<< "Tekan 1 Jika Iya, Tekan Sembarang Jika Tidak : ";cin>>negara;
+      
+      cout<< "\nApakah Anda Membeli Di Bulan Ramadhan?"<<endl;
+      cout<< "Tekan 1 Jika Iya, Tekan Sembarang Jika Tidak : ";cin>>ramadhan;
+    }
+    
+    void toFile()
+	{
+      	write.open("api_data.txt");
+      	write << beli1<< endl;
+      	write << beli2 <<endl;
+      	write << beli3<<endl;
+      	write << beli4<<endl;
+      	write << negara<<endl;
+      	write << ramadhan<<endl;
+      	write.close();
+    }
+  private:
+    ofstream write;
+    int beli1, beli2, beli3, beli4, negara,ramadhan;
+};
---
+++ library/output.h
@@ -0,0 +1,34 @@
+using namespace std;
+
+class Output{
+  	public :
+    void cetak()
+    {
+      cout << "\n=====================================";
+      cout << "\n=\t\tSTRUK PEMBELIAN ANDA\t\t=\n";
+      cout << "=====================================\n";
+     	cout << "Total Biaya Order		: Rp." << data_file[0] << endl;
+      cout << "-------------------------------------" << endl;
+      cout << "\tDiskon Region       : Rp." << data_file[1] << endl;
+      cout << "\tDiskon Ramadhan     : Rp." << data_file[2] << endl;
+     	cout << "\tDiskon Total        : Rp." << data_file[5] << endl;
+      cout << "-------------------------------------" << endl;
+      cout << "Total Tagihan			: Rp." << data_file[4] << endl;
+      cout << "=====================================\n";
+    }
+    void getData()
+    {  
+      	ambil_data.open("api_data.txt");
+      	string t;
+      	while(!ambil_data.eof())
+        {
+         	ambil_data >> data_file[index];
+            index += 1;
+        }
+      	ambil_data.close();
+    }
+  	private :
+    ifstream ambil_data;
+    string data_file[30];
+    int index = 0;
+};
